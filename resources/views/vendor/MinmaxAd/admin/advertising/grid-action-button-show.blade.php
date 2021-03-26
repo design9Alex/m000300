df788e4228d8da5bc449f4aa926770c300422781
design9Alex <48194236+design9Alex@users.noreply.github.com>
2021-03-26 11:35:44 +0800

    update view

---
+++ resources/views/vendor/MinmaxAd/admin/advertising/grid-action-button-show.blade.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var string $id
+ * @var string $uri
+ */
+?>
+{{--
+<a class="btn btn-outline-default btn-sm" role="button" aria-pressed="true" title="@lang('MinmaxBase::admin.grid.actions.view')" href="{{ langRoute("admin.{$uri}.show", ['id' => $id]) }}" target="_blank">
+    <i class="icon-eye3"></i><span class="text-hide">@lang('MinmaxBase::admin.grid.actions.view')</span>
+</a>--}}
